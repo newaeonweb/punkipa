8ee6600f42c027c5b933f572fc66f7c2ace64a19
newaeonweb <fernando@newaeonweb.com.br>
2018-01-29 22:54:13 -0200

    fixed tests

--- src/app/beers/beers.component.spec.ts
+++ src/app/beers/beers.component.spec.ts
@@ -1,14 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BeersComponent } from './games.component';
+import { BeersComponent } from './beers.component';
+import { BeersService } from './services/beers.service';
+import { SearchPipe } from './pipes/search.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
 
-describe('GamesComponent', () => {
+describe('BeersComponent', () => {
   let component: BeersComponent;
   let fixture: ComponentFixture<BeersComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BeersComponent ]
+      imports: [FormsModule, HttpClientModule],
+      declarations: [ BeersComponent, FilterPipe, SearchPipe ],
+      providers: [BeersService]
     })
     .compileComponents();
   }));
--- src/app/beers/services/beers.service.spec.ts
+++ src/app/beers/services/beers.service.spec.ts
@@ -1,11 +1,18 @@
+import { FilterPipe } from './../pipes/filter.pipe';
+import { SearchPipe } from './../pipes/search.pipe';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { BeersService } from './beers.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('BeersService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [BeersService]
+      imports: [HttpClientModule, FormsModule, ReactiveFormsModule],
+      providers: [BeersService],
+      declarations: [SearchPipe, FilterPipe]
     });
   });
 
